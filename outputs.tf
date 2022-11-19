91e49bb2c830e9de35e90e609f5326d431eec53c
Jeremiah Norris <jjno91@gmail.com>
2022-11-18 19:22:37 -0600

    stub out discovery outputs

--- outputs.tf
+++ outputs.tf
@@ -1,44 +1,44 @@
 output "vpc_id" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#id"
-  value       = aws_vpc.this[0].id
+  value       = local.discovered ? null : aws_vpc.this[0].id
 }
 
 output "cidr_block" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#cidr_block"
-  value       = aws_vpc.this[0].cidr_block
+  value       = local.discovered ? null : aws_vpc.this[0].cidr_block
 }
 
 output "all_subnets_ids" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet#id"
-  value       = flatten([aws_subnet.public[*].id, aws_subnet.private[*].id])
+  value       = local.discovered ? null : flatten([aws_subnet.public[*].id, aws_subnet.private[*].id])
 }
 
 output "public_subnets_ids" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet#id"
-  value       = aws_subnet.public[*].id
+  value       = local.discovered ? null : aws_subnet.public[*].id
 }
 
 output "private_subnets_ids" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet#id"
-  value       = aws_subnet.private[*].id
+  value       = local.discovered ? null : aws_subnet.private[*].id
 }
 
 output "all_route_table_ids" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table#id"
-  value       = flatten([aws_route_table.public[0].id, aws_route_table.private[*].id])
+  value       = local.discovered ? null : flatten([aws_route_table.public[0].id, aws_route_table.private[*].id])
 }
 
 output "public_route_table_id" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table#id"
-  value       = aws_route_table.public[0].id
+  value       = local.discovered ? null : aws_route_table.public[0].id
 }
 
 output "private_route_table_ids" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table#id"
-  value       = aws_route_table.private[*].id
+  value       = local.discovered ? null : aws_route_table.private[*].id
 }
 
 output "nat_public_ips" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip#public_ip"
-  value       = aws_eip.this[*].public_ip
+  value       = local.discovered ? null : aws_eip.this[*].public_ip
 }
